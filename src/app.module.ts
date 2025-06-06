c9aa9965d7447e6ab361e65de610b31694b0e221
Volodymyr Berestenko <koltak2mail.ru@gmail.com>
2025-06-06 10:02:03 +0200

    clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { PostModule } from "./post/post.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { UserModule } from "./user/user.module";
 
 @Module({
   imports: [UserModule, PostModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
